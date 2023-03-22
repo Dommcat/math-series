2ba31e95a0ecb099b242e03ac1704ca811618a7b
Dominick Martin <dominickbmartin@gmail.com>
2023-03-22 05:23:33 +0000

    night load lab 02 partials

--- math_series/series.py
+++ math_series/series.py
@@ -1,13 +1,19 @@
 def fibonacci(n):
     """sum first two integers, return the nth value in the fibonacci series"""
-    pass
+
+    
+
+print(fibonacci(10))  
+
+pass
 
 
 def lucas(n):
     """returns the nth value in the lucas numbers """
     pass
 
-def sum_series(idx,a=0,b=1):
+
+def sum_series(idx, a=0, b=1):
     """determine which element in the series to print. The two optional parameters will
     have default values of 0 and 1 and will determine the first two values for the series to be produced."""
     pass
